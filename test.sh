930e479273d5cb4076b7f4aedef36c628e8a05e2
furuhama <furuhama@users.noreply.github.com>
2017-10-30 00:18:40 +0900

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 alias gcf='/Users/yusuke/.nvm/versions/node/v6.11.1/lib/node_modules/@google-cloud/functions-emulator/bin/functions'
-echo '-------deploy START...--------'
+echo '--------DEPLOY START----------'
 gcf deploy helloWorld --trigger-http
-echo '---------deploy end-----------'
+echo '---------DEPLOY END-----------'
 echo ''
-echo '------call helloWorld---------'
+echo '-------CALL helloWorld--------'
 gcf call helloWorld
+echo '----------CALL END------------'
 echo ''
 echo ''
-echo '-------------LOGS-------------'
-gcf logs read --limit=100
+echo '------------LOGS--------------'
+gcf logs read
+echo '----------LOGS END------------'
 echo ''
-echo '---------all clear------------'
+echo '----------all CLEAR-----------'
 gcf clear
 
