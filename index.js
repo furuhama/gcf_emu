ff79769cb30c0f272d5e2beae8a4957372f18c67
furuhama <furuhama@users.noreply.github.com>
2017-10-30 10:37:18 +0900

    index.js works well

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var sekigae = require('sekigae');
-var my_val = 'hey, siri';
-console.log(sekigae.my_func2); // DOES NOT WORK
 
 exports.helloWorld = function helloWorld(req, res) {
-  console.log(sekigae.my_func2); // DOES NOT WORK
-  res.send(my_val); // WORK
+
+  console.log(sekigae.my_func); // WORK
+  console.log(sekigae.my_func2); // WORK
+  res.send(sekigae.my_func2); // WORK
 };
