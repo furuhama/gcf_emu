ea9728228a13d144eb0ae9b901f5a44699fe1ba8
furuhama <furuhama@users.noreply.github.com>
2017-10-31 22:52:14 +0900

    refactor to move text generating fuctions into sekigae/index.js

--- index.js
+++ index.js
@@ -4,12 +4,5 @@ var Sekigae = require('sekigae');
 var s = new Sekigae();
 
 exports.helloWorld = function helloWorld(req, res) {
-  if (s.checkHoliday()) {
-    res.send('It is holiday!!');
-  } else {
-    var memberList = s.getYaml(s.yamlName);
-    var randomArray = s.getRandomArray(15);
-
-    res.send(s.setMemberDeskHash(memberList, randomArray));
-  };
+  res.send(s.makeMainText());
 };
