1bf77c14e5e6b099e43626e8d3ab05ba884357fc
furuhama <furuhama@users.noreply.github.com>
2017-10-31 15:49:18 +0900

    define getToday()

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 var Sekigae = require('sekigae');
 var s = new Sekigae();
 
+// exports.helloWorld = function helloWorld(req, res) {
+//   console.log(s.my_func1()); // WORK
+//   console.log(s.my_func2()); // WORK
+//   console.log(Sekigae.my_func3) // WORK
+//   res.send(s.my_func1()); // WORK
+// };
+
 exports.helloWorld = function helloWorld(req, res) {
-  console.log(s.my_func1()); // WORK
-  console.log(s.my_func2()); // WORK
-  console.log(Sekigae.my_func3) // WORK
-  res.send(s.my_func1()); // WORK
+  res.send(s.getToday(0));
 };
