d79aa862e44cc5b9be313a3816c73849fb09a885
furuhama <furuhama@users.noreply.github.com>
2017-10-31 15:10:05 +0900

    add comment

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var s = new Sekigae();
 exports.helloWorld = function helloWorld(req, res) {
   console.log(s.my_func1()); // WORK
   console.log(s.my_func2()); // WORK
-  console.log(Sekigae.my_func3)
+  console.log(Sekigae.my_func3) // WORK
   res.send(s.my_func1()); // WORK
 };
