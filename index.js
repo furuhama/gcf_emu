79d71e638320e5148039899cb1b6e2ff303775fa
furuhama <furuhama@users.noreply.github.com>
2017-11-01 23:29:00 +0900

    webhook request success!!

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
 var Sekigae = require('sekigae');
 var s = new Sekigae();
 
+var request = require('request');
+var options = {
+  url: 'YOUR-URL-HERE',
+  method: 'POST',
+  headers: { 'Content-Type' : 'application/json' },
+  json: true,
+  body: { 'text' : 'konichiwa' }
+};
+
 exports.helloWorld = function helloWorld(req, res) {
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body.name);
+    } else {
+      console.log('error: ' + response.statusCode)
+    };
+  });
+
+  console.log(process.env.SLACK_URL);
+
   res.send(s.makeMainText());
 };
