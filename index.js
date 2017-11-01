1928848d9f589c65be51fd679576f80d59b2bea8
furuhama <furuhama@users.noreply.github.com>
2017-11-02 00:03:57 +0900

    refactor sendPostRequest method

--- index.js
+++ index.js
@@ -2,26 +2,11 @@
 
 var Sekigae = require('sekigae');
 var s = new Sekigae();
-
-var request = require('request');
-var options = {
-  url: 'YOUR-URL-HERE',
-  method: 'POST',
-  headers: { 'Content-Type' : 'application/json' },
-  json: true,
-  body: { 'text' : 'konichiwa' }
-};
+var url = s.url;
+var text = s.getText();
 
 exports.helloWorld = function helloWorld(req, res) {
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body.name);
-    } else {
-      console.log('error: ' + response.statusCode)
-    };
-  });
-
-  console.log(process.env.SLACK_URL);
+  s.sendPostRequest(url, text);
 
   res.send(s.makeMainText());
 };
